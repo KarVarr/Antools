f845ef3b590c80104d7377346dbbd9877b28b0a0
Karen Vardanian <karenvardanian@MacBook-Air-Karen.local>
2022-06-21 21:50:13 +0300

    slider js

--- js/main.js
+++ js/main.js
@@ -2,6 +2,11 @@
 
 const logoTop = document.querySelector(".footer__logo--img");
 const like = document.querySelectorAll(".like");
+//slider
+const slider = document.querySelector('.slider__slider')
+const sliderBtnNext = document.querySelector('.fa-angle-right')
+const sliderBtnPrev = document.querySelector('.fa-angle-left')
+const activeSLider = document.querySelectorAll(".slider__points--point");
 
 logoTop.addEventListener("click", () => {
   window.scrollTo(0, 0);
@@ -16,4 +21,53 @@ likes.addEventListener('click', function() {
     }
 })
    
+//SLIDER
+let offset = 0;
 
+sliderBtnNext.addEventListener("click", () => {
+  offset = offset - 100;
+  if (offset < -100) {
+    offset = 100;
+  }
+  slider.style.left = `${offset}%`;
+
+
+//---------
+//   activeSLider.forEach(panel => {
+
+//       remove();
+//       panel.classList.add("activeSlider");
+    
+//   });
+
+//   function remove() {
+//     activeSLider.forEach(panel => {
+//       panel.classList.remove("activeSlider");
+//     });
+//   }
+//----------
+
+
+});
+
+sliderBtnPrev.addEventListener('click', () => {
+    offset = offset + 100;
+    if(offset > 100) {
+        offset = -100;
+    }
+    slider.style.left = `${offset}%`;
+})
+
+//SLIDER POINTS
+activeSLider.forEach(panel => {
+    panel.addEventListener("click", () => {
+      remove();
+      panel.classList.add("activeSlider");
+    });
+})
+
+function remove () {
+    activeSLider.forEach(panel => {
+        panel.classList.remove('activeSlider')
+    })
+}
